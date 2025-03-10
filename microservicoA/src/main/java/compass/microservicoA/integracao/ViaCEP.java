9aa5b4fe53a964696ac9bf023f2a1968cc818e01
Jean Mallmann <jean.mallmann.pb@compasso.com.br>
2025-03-10 17:15:42 -0300

    feat: API 1: adicionada a função de listar Eventos por ordem alfabética.

--- microservicoA/src/main/java/compass/microservicoA/integracao/ViaCEP.java
+++ microservicoA/src/main/java/compass/microservicoA/integracao/ViaCEP.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "viacep", url = "https://viacep.com.br/ws/")
-public interface ViaCEP
+@FeignClient(name = "viacep", url = "${viacep.url}")
+public interface ViaCEP 
 {
   @GetMapping("{cep}/json/")
   ViaCEPResposta consultarCEP(@PathVariable("cep") String cep);
